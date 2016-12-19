9c49852cbbde896e3f484b5ef3a6941a70d21f65
paola <paolatorobejar@gmail.com>
2016-12-19 16:09:02 -0500

    fin ejercicio guiado

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 ejercicio 1
 Inicio: Suma de dos números
 ## Ingreso de datos
--
--
--
+- numero1
+- numero2
+- resultado
 
-##Procesos
+## Procesos
+- pedir numero1
+- pedir numero2
+- sumar numero1 con numero2 y almacenar el rersultado
 
---
+++ main.js
@@ -0,0 +1,10 @@
+	
+	/*var nombre="karla"
+		alert("Hola"+ nombre)*/
+	var numero1, numero2, resultado
+	numero1 = prompt("por favor , ingresa un numero")
+	/*alert(numero1);*/
+	numero2 = prompt("por favor, ingresa oro numero")
+
+	resultado= parseInt(numero1) + parseInt(numero2)
+	alert(resultado);
